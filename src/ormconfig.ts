7e88a25d464caa6794a90a29f585170cae51e90d
JZhen9 <jess081940217@gmail.com>
2023-04-29 12:42:08 +0800

    修改MySQL連線Options

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,13 +1,37 @@
 import { ConnectionOptions } from 'typeorm'
 import { User } from './entity/User'
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const connectionOptions: ConnectionOptions = {
-    type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'backend_test_with_orm_4109',
-  entities: [User],
+const {
+  MYSQL_HOST,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+  MYSQL_PORT,
+  MYSQL_ENTITIES,
+} = process.env;
+
+if (
+  !MYSQL_HOST ||
+  !MYSQL_USER ||
+  !MYSQL_PASSWORD ||
+  !MYSQL_DATABASE ||
+  !MYSQL_PORT ||
+  !MYSQL_ENTITIES
+) {
+  console.error('Missing environment variables for MySQL connection');
+  process.exit(1);
+}
+
+export const connectionOptions: ConnectionOptions =
+{
+  type: 'mysql',
+  host: MYSQL_HOST,
+  port: parseInt(MYSQL_PORT, 10),
+  username: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  database: MYSQL_DATABASE,
+  entities: [MYSQL_ENTITIES],
   synchronize: true,
 }
